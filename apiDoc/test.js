0579e794cf8b3bd5dbdbb0781d930693894e051c
SEARteam1928 <halitulla@gmail.com>
2020-02-13 22:39:18 +0500

    'timelessonadded'

--- apiDoc/test.js
+++ apiDoc/test.js
@@ -7,6 +7,13 @@ var day = date.getDay();
 if (day === 0) {
     day = 1;
 }
+/*
+" 8:15 - 9:00\n 2| 9:00 - 9:45\n\nперемена 15 минут\n\n 3| 10:00 - 10:45\n 4| 10:45 - 11:30\n\nперемена 30 минут\n\n 5| 12:00 - 12:45\n 6| 12:45 - 13:30\n\nперемена 30 минут\n\n 7| 14:00 - 14:45\n 8| 14:45 - 15:30\n\nперемена 15 минут\n\n 9| 15:45 - 16:30\n10| 16:30 - 17:15\n\nперемена 10 минут\n\n11| 17:25 - 18:10\n12| 18:10 - 18:55\n\n"
+"Cуббота\n\n\n 1| 8:15 - 9:00\n 2| 9:00 - 9:45\n\nперемена 5 минут\n\n3| 9:50 - 10:35\n 4| 10:35 - 11:20\n\nперемена 30 минут\n\n 5| 11:50 - 12:35\n 6| 12:35 - 13:20\n\nперемена 10 минут\n\n 7| 13:30 - 14:15\n 8| 14:15 - 15:00\n\nперемена 10 минут\n\n 9| 15:10 - 15:55\n10| 15:55 - 16:40\n\nперемена 5 минут\n\n11| 16:45 - 17:30\n12| 17:30 - 18:15\n\n"
+*/
+
+const timeLessonMnFr = ["","8:15 - 9:00", "9:00 - 9:45", "10:00 - 10:45", "10:45 - 11:30", "12:00 - 12:45", "12:45 - 13:30", "14:00 - 14:45", "14:45 - 15:30", "15:45 - 16:30", "16:30 - 17:15", "17:25 - 18:10", "18:10 - 18:55"];
+const timeLessonSt = ["","8:15 - 9:00", "9:00 - 9:45", "9:50 - 10:35", "10:35 - 11:20", "11:50 - 12:35", "12:35 - 13:20", "13:30 - 14:15", "14:15 - 15:00", "15:10 - 15:55", "15:55 - 16:40", "16:45 - 17:30", "17:30 - 18:15"];
 
 function getOptionsList() {
     $.ajax({
@@ -52,8 +59,10 @@ function updateTimetableList(timetableOn) {
         success(data) {
             console.log(data);
             allTable.innerHTML = "";
-            for(let i = 0; i < data.timetable.length; i++){
-                createTableLine(data,i);
+            userInfo.innerHTML = "";
+            userInfo.innerHTML = data.timetable[0].groupname + " " + data.timetable[0].dayOfWeek;
+            for (let i = 0; i < data.timetable.length; i++) {
+                createTableLine(data, i);
             }
         },
         error(header, textError) {
@@ -64,17 +73,27 @@ function updateTimetableList(timetableOn) {
     });
 }
 
-function createTableLine(data,numSubject) {
+function createTableLine(data, numSubject) {
     let dive = document.createElement('div');
 
-    dive.setAttribute("class","tableLine");
+    dive.setAttribute("class", "tableLine");
 
-    dive.innerHTML  ="<div class='dayOfWeek'>" + data.timetable[numSubject].day + "</div>"
-        +"<div>" + data.timetable[numSubject].groupname + "</div>"
-        + "<div> номер урока " + data.timetable[numSubject].hour + "</div>"
-        + "<div> подгруппа " + data.timetable[numSubject].subgroup +"</div>"
-        + "<div>" + data.timetable[numSubject].subject +" </div>"
-        + "<div>" + data.timetable[numSubject].teacher +"</div>";
+    if (day === "6") {
+        dive.innerHTML = "<br><div class='numLesson'> номер урока " + data.timetable[numSubject].numLesson + "</div>"
+            + "<div class='subGroup'> подгруппа " + data.timetable[numSubject].subgroup + "</div>"
+            + "<div class='subject'>" + data.timetable[numSubject].subject + " </div>"
+            + "<div class='teacher'>" + data.timetable[numSubject].teacher + "</div>"
+            + "<div class='timeLesson'>" + timeLessonSt[data.timetable[numSubject].numLesson] + "</div>"
+            + "<br>";
+    } else {
+        dive.innerHTML = "<br><div class='numLesson'> номер урока " + data.timetable[numSubject].numLesson + "</div>"
+            + "<div class='subGroup'> подгруппа " + data.timetable[numSubject].subgroup + "</div>"
+            + "<div class='subject'>" + data.timetable[numSubject].subject + " </div>"
+            + "<div class='teacher'>" + data.timetable[numSubject].teacher + "</div>"
+            + "<div class='timeLesson'>" + timeLessonMnFr[data.timetable[numSubject].numLesson] + "</div>"
+            + "<br>";
+    }
+
     allTable.append(dive);
 }
 
